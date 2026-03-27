d62025f4becd916cbd76c0f721ffeeebfaa77f35
NLIPPI <nlippilira@gmail.com>
2026-03-27 20:54:55 -0300

    Fix rutas frontend GitHub Pages

--- frontend/app.js
+++ frontend/app.js
@@ -15,7 +15,7 @@ form.addEventListener("submit", async (e) => {
     };
 
     try {
-        const response = await fetch("http://127.0.0.1:5000/predict", {
+        const response = await fetch("https://smartpredict-sn4v.onrender.com/predict", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
